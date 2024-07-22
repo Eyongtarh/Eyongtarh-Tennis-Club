96c9b567042f577f9b072974e1bb95caaade9b26
Augustine Eyong <eyongtarhb@yahoo.co.uk>
2024-07-22 00:20:24 +0000

    add link of script.js to index.html file

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let navLink = document.getElementById("nav-bar");
+
+        function showMenu() {
+            navLink.style.right = "0";
+        }
+
+        function hideMenu() {
+            navLink.style.right = "-200px";
+        }
+
