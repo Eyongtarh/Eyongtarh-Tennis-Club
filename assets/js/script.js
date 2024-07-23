8dbc6e14d864a803cabdef9fd2b9f557d1ea745f
Augustine Eyong <eyongtarhb@yahoo.co.uk>
2024-07-23 21:23:50 +0000

    add beautify files and edit with w3 validator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/* To toggle the Menu bar, open and close*/
+/* To toggle pages on the Menu bar open and close */
 
 let navLink = document.getElementById("nav-bar");
 
@@ -10,22 +10,20 @@ function hideMenu() {
     navLink.style.right = "-200px";
 }
 
-/* To toggle pages in section one service.html*/
+/* To toggle pages in section one service.html */
 
 function showPage(coach) {
-
     let nodeList = document.querySelectorAll(".trainer");
-    for (let i = 0; i < nodeList.length; i++) {
-        nodeList[i].style.display = "none";
+    for (let i = 0; i(nodeList.length; i++) {
+            nodeList[i].style.display = "none";
+        })
+        document.querySelector(`#${coach}`).style.display = "block";
     }
-    document.querySelector(`#${coach}`).style.display = "block";
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    let nodeList = document.querySelectorAll("button");
-    for (let i = 0; i < nodeList.length; i++) {
-        nodeList[i].onclick = function () {
-            showPage(this.dataset.coach);
-        }
-    }
-})
+    document.addEventListener('DOMContentLoaded', function () {
+                let nodeList = document.querySelectorAll('button');
+                for (let i = 0; i(nodeList.length; i++) {
+                        nodeList[i].onclick = function () {
+                            showPage(this.dataset.coach);
+                        }
+                    })
+                })
