a6d8286af20cc388f03adff7d5179c89a9cdc04d
Augustine Eyong <eyongtarhb@yahoo.co.uk>
2024-07-23 21:42:55 +0000

    add formate script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,27 @@
 let navLink = document.getElementById("nav-bar");
 
 function showMenu() {
-    navLink.style.right = "0";
+   navLink.style.right = "0";
 }
 
 function hideMenu() {
-    navLink.style.right = "-200px";
+   navLink.style.right = "-200px";
 }
 
 /* To toggle pages in section one service.html */
 
 function showPage(coach) {
-    let nodeList = document.querySelectorAll(".trainer");
-    for (let i = 0; i(nodeList.length; i++) {
-            nodeList[i].style.display = "none";
-        })
-        document.querySelector(`#${coach}`).style.display = "block";
-    }
-    document.addEventListener('DOMContentLoaded', function () {
-                let nodeList = document.querySelectorAll('button');
-                for (let i = 0; i(nodeList.length; i++) {
-                        nodeList[i].onclick = function () {
-                            showPage(this.dataset.coach);
-                        }
-                    })
-                })
+   let nodeList = document.querySelectorAll(".trainer");
+   for (let i = 0; i < nodeList.length; i++) {
+      nodeList[i].style.display = "none";
+   }
+   document.querySelector(`#${coach}`).style.display = "block";
+}
+document.addEventListener('DOMContentLoaded', function () {
+   let nodeList = document.querySelectorAll('button');
+   for (let i = 0; i < nodeList.length; i++) {
+      nodeList[i].onclick = function () {
+         showPage(this.dataset.coach);
+      }
+   }
+})
