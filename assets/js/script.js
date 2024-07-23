74ba3b04a23a85543617ff0456a9149f40ffeece
Augustine Eyong <eyongtarhb@yahoo.co.uk>
2024-07-23 00:45:45 +0000

    add eventlistener to toggle pages in section on service.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 let navLink = document.getElementById("nav-bar");
 
-        function showMenu() {
-            navLink.style.right = "0";
-        }
+function showMenu() {
+    navLink.style.right = "0";
+}
 
-        function hideMenu() {
-            navLink.style.right = "-200px";
-        }
+function hideMenu() {
+    navLink.style.right = "-200px";
+}
 
+function showPage(coach){
+
+let nodeList = document.querySelectorAll(".trainer");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.display = "none";
+}
+        document.querySelector(`#${coach}`).style.display = "block";
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+    let nodeList = document.querySelectorAll("button");
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].onclick = function () {
+            showPage(this.dataset.coach);
+                            }
+                        }
+                    })
