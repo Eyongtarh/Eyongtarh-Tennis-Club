7afdfcbad3e93bcc511c1f5bf2497ea0f408a4f1
Augustine Eyong <eyongtarhb@yahoo.co.uk>
2024-07-26 19:49:19 +0000

    add wrote code for form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ function showMenu() {
 
 function hideMenu() {
     navLink.style.right = "-200px";
-}
+};
 
 /* To toggle pages in section one service.html */
 
@@ -26,4 +26,68 @@ document.addEventListener('DOMContentLoaded', function () {
             showPage(this.dataset.coach);
         }
     }
-})
+});
+
+/* Book Time form validation */
+
+const form = document.getElementById("booktime");
+
+form.addEventListener("submit",(event)=> {
+
+    const nameInputf = document.getElementById("first_name");
+    const nameInputl = document.getElementById("last_name");
+    const passWord1 = document.getElementById("userpass1");
+    const passWord2 = document.getElementById("userpass2");
+    const emailInput = document.getElementById("email_address");
+    const nameErrorf = document.getElementById("namef-error");
+    const nameErrorl = document.getElementById("namel-error");
+    const pass1Error = document.getElementById("pass1-error");
+    const pass2Error = document.getElementById("pass2-error");
+    const emailError = document.getElementById("email_error");
+
+    nameErrorf.textContent = "";
+    nameErrorl.textContent = "";
+    pass1Error.textContent = "";
+    pass2Error.textContent = "";
+    emailError.textContent = "";
+    nameInputf.classList.remove("error");
+    nameInputl.classList.remove("error");
+    passWord1.classList.remove("error");
+    passWord2.classList.remove("error");
+    emailInput.classList.remove("error");
+
+    if (nameInputf.value.trim() === "") {
+        nameErrorf.textContent = "Please enter your first name.";
+        nameInputf.classList.add("error");
+        event.preventDefault();
+    }
+
+    if (passWord1.value.trim() === "") {
+        pass1Error.textContent = "Please enter your last name.";
+        passWord1.classList.add("error");
+        event.preventDefault();
+    }
+
+    if (passWord2.value.trim() === "") {
+        pass2Error.textContent = "Please enter your last name.";
+        passWord2.classList.add("error");
+        event.preventDefault();
+    }
+
+    if (nameInputf.value.trim() === "") {
+        nameErrorf.textContent = "Please enter your first name.";
+        nameInputf.classList.add("error");
+        event.preventDefault();
+    }
+
+    const emailValue = emailInput.value.trim();
+    if (emailValue === "") {
+        emailError.textContent = "Please enter your email address.";
+        emailInput.classList.add("error");
+        event.preventDefault();
+    } else if (!/^\S+@\S+\.\S+$/.test(emailValue)) {
+        emailError.textContent = "Please enter a valid email address.";
+        emailInput.classList.add("error");
+        event.preventDefault();
+    }
+});
