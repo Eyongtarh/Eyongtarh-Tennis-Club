7daa8ed24116a8765fa71e7ea2bcb312e106fc34
Augustine Eyong <eyongtarhb@yahoo.co.uk>
2024-08-02 19:30:15 +0000

    add comments in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
-/* To toggle pages on the Menu bar open and close */
-
+/* Decleration of variable To toggle the Menu bar open and close */
 const navBar = document.getElementById("nav-bar");
+/* Decleration of variable To toggle pages in main section one pasge index.html/service.html */
+const nodeList = document.querySelectorAll(".trainer");
 
+/* Decleration of variable To toggle color on Read More anchor in header */
+const readMore = document.getElementById("readm");
+const readmColor = ["blue", "green"];
+/* Decleration of variable To validate form in book.html */
+const form = document.getElementById("booktime");
+const nameInputf = document.getElementById("first_name");
+const nameInputl = document.getElementById("last_name");
+const passWord1 = document.getElementById("userpass1");
+const passWord2 = document.getElementById("userpass2");
+const emailInput = document.getElementById("email_address");
+const serviceInput = document.getElementById("service-type");
+const nameErrorf = document.getElementById("namef-error");
+const nameErrorl = document.getElementById("namel-error");
+const pass1Error = document.getElementById("pass1-error");
+const pass2Error = document.getElementById("pass2-error");
+const emailError = document.getElementById("email-error");
+const serviceError = document.getElementById("service-error");
+
+
+/* Decleration of variable To toggle the Menu bar open and close */
+
+/* To toggle the Menu bar open and close */
 function showMenu() {
     navBar.style.right = "0";
 }
@@ -14,7 +37,6 @@ function hideMenu() {
 
 function showPage(coach) {
     /*select all the pages not to display*/
-    const nodeList = document.querySelectorAll(".trainer");
     for (let i = 0; i < nodeList.length; i++) {
         nodeList[i].style.display = "none";
     }
@@ -23,8 +45,7 @@ function showPage(coach) {
 /*select the list to display on button click*/
 document.addEventListener('DOMContentLoaded', function () {
     const nodeList = document.querySelectorAll("button");
-    /*Here's an example of a for loop that prints every third number from 0 up 
-    to and including 99 using console.log in JavaScript:*/
+    /*To display a page in the list using this javascript on click of the button */
     for (let i = 0; i < nodeList.length; i++) {
         nodeList[i].onclick = function () {
             showPage(this.dataset.coach);
@@ -57,77 +78,62 @@ function color4() {
 
 
 /* Toggle color on Read More anchor in header */
-
-const readMore = document.getElementById("readm");
-const readmColor = ["blue", "green"];
 setInterval(function () {
+    /* selects the color at random in the list of colors and set as backgrond every second*/
     let randomIndex = Math.floor(Math.random() * readmColor.length);
     readMore.style.background = readmColor[randomIndex];
 }, 1000);
 
 
 /* Book Time form validation */
-
-const form = document.getElementById("booktime");
-
 form.addEventListener("submit", (event) => {
-    const nameInputf = document.getElementById("first_name");
-    const nameInputl = document.getElementById("last_name");
-    const passWord1 = document.getElementById("userpass1");
-    const passWord2 = document.getElementById("userpass2");
-    const emailInput = document.getElementById("email_address");
-    const serviceInput = document.getElementById("service-type");
-    const nameErrorf = document.getElementById("namef-error");
-    const nameErrorl = document.getElementById("namel-error");
-    const pass1Error = document.getElementById("pass1-error");
-    const pass2Error = document.getElementById("pass2-error");
-    const emailError = document.getElementById("email-error");
-    const serviceError = document.getElementById("service-error");
-
+    /* Error messages for each input*/
     nameErrorf.textContent = "";
     nameErrorl.textContent = "";
     pass1Error.textContent = "";
     pass2Error.textContent = "";
     emailError.textContent = "";
     serviceError.textContent = "";
+    /* Removes Error messages for each input*/
     nameInputf.classList.remove("error");
     nameInputl.classList.remove("error");
     passWord1.classList.remove("error");
     passWord2.classList.remove("error");
     emailInput.classList.remove("error");
     serviceInput.classList.remove("error");
-
+/* if first name value is empty, withh all spaces removed, error message will display and form validation is prevented */
     if (nameInputf.value.trim() === "") {
         nameErrorf.textContent = "Please enter your first name.";
         nameInputf.classList.add("error");
         event.preventDefault();
     }
-/* // Base case,, Remove leading and trailing spaces*/
+/* if last name value is empty, withh all spaces removed, error message will display and form validation is prevented */
     if (nameInputl.value.trim() === "") {
-        /*Recursive case.  ,, Remove all spaces */
         nameErrorl.textContent = "Please enter your last name.";
         nameInputl.classList.add("error");
         event.preventDefault();
     }
-
+/* if password1 value is empty, withh all spaces removed, error message will display and form validation is prevented */
     if (passWord1.value.trim() === "") {
         pass1Error.textContent = "Please enter your password.";
         passWord1.classList.add("error");
         event.preventDefault();
     }
-
+/* if password2 value is empty, withh all spaces removed, error message will display and form validation is prevented */
     if (passWord2.value.trim() === "") {
         pass2Error.textContent = "Please re-enter your password.";
         passWord2.classList.add("error");
         event.preventDefault();
     }
-
+/* if password1 is not equal to pasword2 value is empty, withh all spaces removed, error message 
+will display and form validation is prevented */
     if (passWord1.value.trim() !== passWord2.value.trim()) {
         pass2Error.textContent = "Please passwords doesn't match.";
         passWord2.classList.add("error");
         event.preventDefault();
     }
-
+/* if email value is empty, withh all spaces removed, error message will display and form validation is prevented 
+and if its filled with wrong formate, error message will display and form validation is prevented*/
     const emailValue = emailInput.value.trim();
     if (emailValue === "") {
         emailError.textContent = "Please enter your email address.";
@@ -138,7 +144,7 @@ form.addEventListener("submit", (event) => {
         emailInput.classList.add("error");
         event.preventDefault();
     }
-
+/* if service value is empty, withh all spaces removed, error message will display and form validation is prevented */
     if (serviceInput.value.trim() === "") {
         serviceError.textContent = "Please choose a service.";
         serviceInput.classList.add("error");
